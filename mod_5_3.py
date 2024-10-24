9db5a27fdb5715acf1f8c2af886bbb938355bdf1
Firdavs82 <kamfirdavss@gmail.com>
2024-10-24 11:43:37 +0300

    Add files via upload

--- mod_5_3.py
+++ mod_5_3.py
@@ -18,34 +18,42 @@ class House:
         return(f'Название: {self.name}, количество этажей: {self.number_of_floors}')
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
 
     def __add__(self, value):
-
-        self.number_of_floors += value
+        if isinstance(value, int):
+            self.number_of_floors += value
         return self
 
     def __iadd__(self, value):
-        self.number_of_floors += value
+        if isinstance(self, House):
+            self.number_of_floors += value
         return self
 
     def __radd__(self, value):
-        self.number_of_floors = value + self.number_of_floors
+        if isinstance(self, House):
+            self.number_of_floors = value + self.number_of_floors
         return self
 
 
@@ -54,15 +62,16 @@ h2 = House('ЖК Акация', 20)
 
 print(h1)
 print(h2)
-print()
+
 print(h1 == h2) # __eq__
 h1 = h1 + 10 #__add__|
 print(h1)
+
 print(h1 == h2)
-print()
+
 h1 += 10 #__iadd__
 print(h1)
-print()
+
 h2 = 10 + h2
 print(h2)
 print()
