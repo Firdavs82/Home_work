3ac8583c1cb459b45cc6ce655ca362c024be444a
Firdavs82 <kamfirdavss@gmail.com>
2024-11-28 14:49:09 +0300

    Add files via upload

--- mod_9_6.py
+++ mod_9_6.py
@@ -1,9 +1,7 @@
 def all_variants(text):
     for i in range(len(text)):
-        yield text[i:  i + 1]
-
-    for j in range(len(text[])):
-        yield text[i: j + i + 1]
+        for j in range(len(text[])):
+            yield text[i: j + i + 1]
 
 a = all_variants('abc')
 for i in a:
