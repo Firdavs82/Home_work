af70434db16a6ff83b6bddb23cf46fa7147883eb
Firdavs82 <kamfirdavss@gmail.com>
2024-12-10 13:25:19 +0300

    Add files via upload

--- mod_10_5.py
+++ mod_10_5.py
@@ -1,20 +1,21 @@
 import multiprocessing
 from datetime import datetime
+import codecs
 
 def read_info(name):
     all_data = []
-    with open(name, 'r') as file:
+    with open(name, 'r', encoding='utf_8_sig') as file:
         for line in file:
             all_data.append(line)
 
 
 
 if __name__ == '__main__':
-    files = [f'./file {number}.txt' for number in range(1, 5)]
+    filenames = [f'./file{number}.txt' for number in range(1, 5)]
 
     start_time = datetime.now()
-    for name in files:
-        read_info(name)
+    for i in filenames:
+        read_info(i)
     end_time = datetime.now()
     print(end_time - start_time, '(линейный)')
 
@@ -22,7 +23,7 @@ if __name__ == '__main__':
     start_time2 = datetime.now()
 
     with multiprocessing.Pool(processes=4) as pool:
-        pool.map(read_info, files)
+        pool.map(read_info, filenames)
 
     end_time2 = datetime.now()
     time_of_multiprocessing = end_time2 - start_time2
