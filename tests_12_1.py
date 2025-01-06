6b5202f26533473ee22cf318541ed9c6278a0a36
Firdavs82 <kamfirdavss@gmail.com>
2025-01-06 17:57:29 +0300

    Add files via upload

--- tests_12_1.py
+++ tests_12_1.py
@@ -1,32 +1,36 @@
-import mod_12_1
+from mod_12_1 import Runner
 import unittest
 
 class RunnerTest(unittest.TestCase):
+    is_frozen = False
 
-    def test_wolk(self):
+
+    def test_walk(self):
 
         """Test method walk in mod_12_1"""
 
-        walker = mod_12_1.Runner("Пешеход")
+        walker = Runner("Пешеход")
         for i in range(10):
             walker.walk()
         self.assertEqual(walker.distance, 50)
+
 
     def test_run(self):
 
         """Test method run in mod_12_1"""
 
-        runner = mod_12_1.Runner("Бегун")
+        runner = Runner("Бегун")
         for i in range (10):
             runner.run()
         self.assertEqual(runner.distance, 100)
+
 
     def test_challenge(self):
 
         """Test two object"""
 
-        man = mod_12_1.Runner('Tom')
-        woman = mod_12_1.Runner('Katty')
+        man = Runner('Tom')
+        woman = Runner('Katty')
         for i in range(10):
             man.run()
             woman.walk()
